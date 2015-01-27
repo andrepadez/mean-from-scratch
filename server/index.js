9c0c04da6b054f87edbd7ce478ec76d13908973e
André Alçada Padez <andre.padez@gmail.com>
2015-01-28 00:23:42 +0100

    simple swig usage

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
 var express = require('express');
-var app = express();
+var swig = require('swig');
 
+var app = express();
 var port = 3000;
 
+
+
+//sets the listening port to 3000, if there is none configured as an environment variable
+app.set('port', process.env.PORT || port);
+//Sets up Swig as the templating engine
+app.set('views', 'views/');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+swig.setDefaults({
+    cache: false ,
+    autoescape: false
+});
+
+    
+
+
 app.get('/', function(req, res){
-    res.send('welcome');
+
+    //call to controller
+    var data = {
+        qty: 5,
+        names: ['a', 'b', 'c', 'd']
+    };
+
+    res.render('index', data);
 });
 
 app.listen(port, function(){
     console.log('listening on port ', port);
 });
+
+
